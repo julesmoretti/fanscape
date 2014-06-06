f10120e473a931f8688df41b5ecafe6450590a3d
julesmoretti <julesmoretti@me.com>
2014-06-06 14:22:52 -0700

    globe work with current user

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,6 @@ var instagramUtils = require('./instagramUtils.js');
 // exports.route = route;
 module.exports = function(app, passport) {
   app.get('/', instagramUtils.loadPage);
-  // app.get('/globe', instagramUtils.fetchAllMedia);
-  app.get('/globe', instagramUtils.temp);
+  app.get('/globe', instagramUtils.fetchAllMedia);
+  // app.get('/globe', instagramUtils.temp);
 };
