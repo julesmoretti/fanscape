f5b16435ec0b7599872f55ba92098e5f2479aa90
julesmoretti <julesmoretti@me.com>
2014-06-16 00:03:52 -0700

    fixed heavy loading... data not perfectly loaded though

--- app/instagramUtils.js
+++ app/instagramUtils.js
@@ -16,7 +16,6 @@ ig.use({
   client_secret: process.env.FANSCAPECLIENTSECRET
   });
 
-
 // FIRST thing loaded '/'
 exports.loadPage = function(req, res) {
   res.render('../views/partials/login.ejs');
@@ -34,6 +33,7 @@ exports.handleauth = function(req, res) {
       console.log(err.body);
       res.send("Didn't work");
     } else {
+      console.log("Did work");
 
       // extract data to be saved later to Firebase
       queryCode           = req.query.code;
@@ -47,78 +47,82 @@ exports.handleauth = function(req, res) {
       // console.log(full_name, "this should be the full name");
       // console.log(userName, "this should be the user Name");
       // console.log(id, "this should be the ID");
-      res.redirect('/globe?user='+userName);
+      res.redirect('/globe?user='+userName+'&id='+id);
     }
   });
   };
 
+
 // FOURTH redirected from handleauth with userName clipped to it.
 exports.fetchAllMedia = function(req, res) {
-  // app.post
-  res.render('../views/partials/globe.ejs');
+  res.render('../views/partials/globe.ejs');  // rendering ejs to html
 
   // user variables
   var followers = [],  //stores users followers ID list in array.
       coordinates = [],  // stores results of image long, lat, mag
+      fullData = [],
+      fullDataMerge = false,
       followerFeeds = [],
       followersCount = 0,
       followersMaxCount,
-      dataChild,
-      userSnapshot;
-
-
-  var theUrl = req.url;
-  var tempArray = theUrl.split("=");
-  userName = tempArray[1];
+      userSnapshot,
+      theUrl = req.url,
+      tempTupple = theUrl.split("&"),
+      tempUser = tempTupple[0].split("="),
+      tempId = tempTupple[1].split("="),
+      userName = tempUser[1],
+      id = tempId[1];
 
   // takes a snapshot of the user name data
   fb.child(userName).once('value', function(snapshot) {
   userSnapshot = snapshot.val();
 
-
-  if(userSnapshot){
-    id = userSnapshot.id;
-  }
-
-    // STEP 2 //
-    // creates array with a list of followers ID called "followers" & a "followersCount" > STEP 3
-    var paginationFollowers = function (err, users, pagination, limit) {
-      console.log("STEP 2 - in the paginationFollowers");
-
+  // STEP 2 //
+  // creates array with a list of followers ID called "followers" & a "followersCount" STEP 3
+  var paginationFollowers = function (err, users, pagination, limit) {
+    // console.log("STEP 2 - in the paginationFollowers");
+    if(err){
+      console.log(err);
+    } else {
       followers = followers.concat(_.pluck(users, 'id')); // keeps adding to followers variable list of followers ID
       // goes through each pagination to add to the followers list.
       if (pagination && pagination.next) {
         pagination.next(paginationFollowers);
       } else {
         // converts the stringed followers to numbers
-        var temp = [];
+        var digitizedFollowers = [];
         for(var i = 0; i < followers.length; i++){
-          temp.push(JSON.parse(followers[i]));
+          digitizedFollowers.push(JSON.parse(followers[i]));
         }
         // done with all pagination
-        follwers = temp;
+        followers = digitizedFollowers;
+        // console.log(followers.length, "followers length");
         checkIntegrity(); // checks integrity of database for existing data
       }
-      };
+    }
+    };
 
-    // STEP 3 //
-    var checkIntegrity = function (err, medias, pagination, limit) {
-      console.log("STEP 3 - in the checkIntegrity");
-
-        // if nothing existing for current user then create information for user
-        if (!userSnapshot){
-        // create a directory with user name and adds the full name to it
-        fb.child(userName).update({'name': full_name}, function(){
-            // adds to the user name the ID
-            fb.child(userName).update({'id': id.toString()}, function(){
-              // adds the token to the library
-              fb.child(userName).update({'token': token.toString()}, function(){
-                // adds the query code to the database
-                fb.child(userName).update({'queryCode': queryCode.toString()}, function(){
-                  // adds the profile photo to the database.
-                  fb.child(userName).update({'profile_picture': profile_picture.toString()}, function(){
-                    // adds the total number of followers fetched
-                    fb.child(userName).child('geoData').child('followers').update({'totalFollowers': followers.length}, function(){
+  // STEP 3 //
+  // creates a user structure with full & user name, id, token, query code, profile picture
+  var checkIntegrity = function () {
+    // console.log("STEP 3 - in the checkIntegrity");
+      // if nothing existing for current user then create information for user
+      if (!userSnapshot){
+      // create a directory with user name and adds the full name to it
+      fb.child(userName).child('userData').update({'name': full_name}, function(){
+          // adds to the user name the ID
+          fb.child(userName).child('userData').update({'id': id.toString()}, function(){
+            // adds the token to the library
+            fb.child(userName).child('userData').update({'token': token.toString()}, function(){
+              // adds the query code to the database
+              fb.child(userName).child('userData').update({'queryCode': queryCode.toString()}, function(){
+                // adds the profile photo to the database.
+                fb.child(userName).child('userData').update({'profile_picture': profile_picture.toString()}, function(){
+                  // SOON TO BE REMOVED //
+                  // adds the total number of followers fetched
+                  fb.child(userName).child('geoData').child('followers').update({'totalFollowers': followers.length}, function(){
+                    // adds the total number of followers fetched inside a pureData folder
+                    fb.child(userName).child('pureData').child('followers').update({'totalFollowers': followers.length}, function(){
                       // since no initial user found just creates all the data
                       goThroughFollowers();
                     });
@@ -127,122 +131,188 @@ exports.fetchAllMedia = function(req, res) {
               });
             });
           });
+        });
 
-        // user was found so check consistency of data by comparing to followers list
-        } else {
-          //TODO check for difference between previous total count...
-          // Do something else
-          fb.child(userName).child('geoData').child('followers').update({'totalFollowers': (followers.length)}, function(){
+      // user was found so check consistency of data by comparing to followers list
+      } else {
+        //TODO check for difference between previous total count...
+        // SOON TO BE REMOVED //
+        fb.child(userName).child('geoData').child('followers').update({'totalFollowers': (followers.length)}, function(){
+          // adds the total number of followers fetched inside a pureData folder
+          fb.child(userName).child('pureData').child('followers').update({'totalFollowers': (followers.length)}, function(){
             // since no initial user found just creates all the data
             goThroughFollowers();
           });
+        });
+      }
+    };
+
+  // STEP 4 //
+  // runs checkIfFollowerExist on every acquired followers
+  var goThroughFollowers = function (){
+    // console.log("STEP 4 - in the goThroughFollowers");
+
+    for (var i = 0; i < followers.length; i++){
+      checkIfFollowerExist(followers[i]);
+    }
+
+    };
+
+  // STEP 5 //
+  // if no followers director then go fetch it using getFollowerMedia
+  var checkIfFollowerExist = function (follower){
+    // console.log("STEP 5 - in the checkIfFollowerExist");
+
+    // check if locations exists
+    if (!userSnapshot){
+      // console.log("since nothing existed creates media");
+      getFollowerMedia(follower);
+    } else if (!userSnapshot.geoData) {
+      // console.log("since no geoData folder exist creates media");
+      getFollowerMedia(follower);
+    } else if (!userSnapshot.geoData[follower]){
+      // console.log("since no follower specific folder exist creates media");
+      getFollowerMedia(follower);
+    } else if (!userSnapshot.geoData[follower].status){
+      // console.log("since no follower status complete exist delete and then creates fresh media");
+      fb.child(userName).child('geoData').child(follower).remove();
+      getFollowerMedia(follower);
+    } else {
+      // console.log(follower+" already exist");
+      followersCount++;
+      fb.child(userName).child('pureData').child('followers').update({'followersDone': (followersCount)});
+      fb.child(userName).child('geoData').child('followers').update({'followersDone': (followersCount)});
+    }
+    };
+
+  // STEP 6 //
+  // ....
+  var getFollowerMedia = function (follower) {
+    // console.log("STEP 6 - in the getFollowerMedia");
+    var followersData = [];
+
+    var extractCoordinates = function (err, medias, pagination, limit) {
+      // console.log("STEP 6b - in the extractCoordinates");
+
+      if(medias){
+        var pureMedia = filterMedia(medias); // filters from the media just the media with Geo Locations.
+
+        if(pureMedia.length > 1){
+          // create a follower with geo location data for that follower
+          fb.child(userName).child('geoData').child(follower).child('data').push(pureMedia, function(){});
+
+          followersData = followersData.concat(pureMedia);
         }
-      };
-
-    // STEP 4 //
-    var goThroughFollowers = function (){
-      console.log("STEP 4 - in the goThroughFollowers");
-
-      for (var i = 0; i < followers.length; i++){
-        checkIfFollowerExist(followers[i]);
-      }
-
-      };
-
-
-    // STEP 5 //
-    var checkIfFollowerExist = function (follower){
-      console.log("STEP 5 - in the checkIfFollowerExist");
-
-      // check if locations exists
-      if (!userSnapshot){
-        console.log("since nothing existed creates media");
-        getFollowerMedia(follower);
-      } else if (!userSnapshot.geoData) {
-        console.log("since no geoData folder exist creates media");
-        getFollowerMedia(follower);
-      } else if (!userSnapshot.geoData[follower]){
-        console.log("since no follower specific folder exist creates media");
-        getFollowerMedia(follower);
-      } else if (!userSnapshot.geoData[follower].status){
-        console.log("since no follower status complete exist delete and then creates fresh media");
-        fb.child(userName).child('geoData').child(follower).remove();
-        getFollowerMedia(follower);
-      } else {
-        console.log(follower+" already exist");
-      }
-      };
-
-    // STEP 6 // ++++++ WEIRD ORDER (IF MEDIA Later...) +++++
-    var getFollowerMedia = function (follower) {
-      console.log("STEP 6 - in the getFollowerMedia");
-
-      var extractCoordinates = function (err, medias, pagination, limit) {
-        // console.log("STEP 6b - in the extractCoordinates");
-
-        if(medias){
-          var pureMedia = filterMedia(medias); // filters from the media just the media with Geo Locations.
-          if(pureMedia){
-            // create a follower with geo location data for that follower
-            fb.child(userName).child('geoData').child(follower).child('data').push(pureMedia, function(){
-            // check's for pagination and repeats the process.
-              if (pagination && pagination.next) {
-                pagination.next(extractCoordinates);
-              } else {
-                console.log("done with pagination of extractCoordinates for - "+ follower);
-                followersCount++;
-                fb.child(userName).child('geoData').child(follower).update({'status': 'complete'});
-                fb.child(userName).child('geoData').child('followers').update({'followersDone': (followersCount)});
-              }
-            });
-          } else {
-            // check's for pagination and repeats the process.
-            if (pagination && pagination.next) {
-              pagination.next(extractCoordinates);
-            } else {
-              console.log("done with pagination of extractCoordinates for - "+ follower);
-              followersCount++;
-              fb.child(userName).child('geoData').child(follower).update({'status': 'complete'});
-              fb.child(userName).child('geoData').child('followers').update({'followersDone': (followersCount)});
-            }
-            console.log("extractCoordinates did not find any geo media on a pagination for - "+ follower);
-          }
+        // check's for pagination and repeats the process.
+        if (pagination && pagination.next) {
+          pagination.next(extractCoordinates);
         } else {
+          // console.log("done with pagination of extractCoordinates for - "+ follower);
           followersCount++;
+
+          //add followers data [lat, long, timestamp] to fullData [lat, long, mag] if followersData has stuff in it
+          if(followersData.length > 1){
+            var a = crossRef(followersData, 0.005);
+            fullData = fullData.concat(a);
+
+            // overwrite data in Firebase
+            if(fullData){
+              fb.child(userName).child('pureData').update({'data': fullData});
+            }
+          }
+
+
+          fb.child(userName).child('pureData').child('followers').update({'followersDone': (followersCount)});
+
           fb.child(userName).child('geoData').child(follower).update({'status': 'complete'});
           fb.child(userName).child('geoData').child('followers').update({'followersDone': (followersCount)});
-          console.log("extractCoordinates had no media to extract for - "+ follower);
         }
-      };
+      } else {
+        followersCount++;
+        fb.child(userName).child('pureData').child('followers').update({'followersDone': (followersCount)});
 
-      ig.user_media_recent(follower.toString(), extractCoordinates);
-
-      };
-
-    // STEP 7 //
-    // takes the media object that has pictures, and geolocation and returns an array of long, lat and mag.
-    var filterMedia = function (feed) {
-      console.log("STEP 7 - in the filterMedia");
-
-      var geoDataObj = _.pluck(feed, "location");  // creates an array of obj with only the location properties from feed
-      var timeStamp = _.pluck(feed, "created_time");  // creates an array of time stamps
-
-      var geoLocations = [];
-      for(var i = 0; i < timeStamp.length; i++ ){
-        if(geoDataObj[i] && geoDataObj[i].latitude){
-          geoLocations.push(Math.round(geoDataObj[i].latitude*10)/10);  // latitude data rounded up to 1 decimal
-          geoLocations.push(Math.round(geoDataObj[i].longitude*10)/10);  // longitude data rounded up to 1 decimal
-          geoLocations.push(JSON.parse(timeStamp[i])); // timeStamp data
-        }
+        fb.child(userName).child('geoData').child(follower).update({'status': 'complete'});
+        fb.child(userName).child('geoData').child('followers').update({'followersDone': (followersCount)});
+        // console.log("extractCoordinates had no media to extract for - "+ follower);
       }
+    };
 
-      return geoLocations;
+    // gets media information for specific follower
+    ig.user_media_recent(follower.toString(), extractCoordinates);
+
+    };
+
+  // STEP 7 //
+  // takes the media object that has pictures with geolocation data and returns an array of [lat, long, timestamp]
+  var filterMedia = function (feed) {
+    // console.log("STEP 7 - in the filterMedia");
+
+    var geoDataObj = _.pluck(feed, "location");  // creates an array of obj with only the location properties from feed
+    var timeStamp = _.pluck(feed, "created_time");  // creates an array of time stamps
+
+    var geoLocations = [];
+    for(var i = 0; i < timeStamp.length; i++ ){
+      if(geoDataObj[i] && geoDataObj[i].latitude){
+        geoLocations.push(Math.round(geoDataObj[i].latitude*10)/10);  // latitude data rounded up to 1 decimal
+        geoLocations.push(Math.round(geoDataObj[i].longitude*10)/10);  // longitude data rounded up to 1 decimal
+        geoLocations.push(JSON.parse(timeStamp[i])); // timeStamp data
+      }
+    }
+
+    return geoLocations;
+    };
+
+  // STEP 8 //
+  // takes array data [lat, long, timestamp or magnityre] and filters it down to an array that has an averaged out magnitude average [lat, long, mag]
+  var crossRef = function(data, magnitude){
+    var result = [],
+        obj    = {};
+
+    if(data && data.length > 1){
+      // creates an object with mean number of repetitions
+      for (var i = 0; i < data.length; i++) {
+        var pair = JSON.stringify([(data[i]),(data[i+1])]);
+
+        // if the pair exists
+        if (obj[pair]){
+
+          // if the pair value is less then 1.5 magnitude
+          if(obj[pair] < 1.5 ){
+
+            // if there is a timestamp add one more magnitude
+            if(data[i+2] > 1000){
+              obj[pair] += magnitude;
+
+            // if there is another magnitude.
+            } else {
+              obj[pair] += (data[i+2]);
+            }
+          }
+        } else {
+          obj[pair] = magnitude;
+        }
+        i++;
+        i++;
       };
 
-    // STEP 1 //
-    // created or overwrite existing folder structure
-    console.log("STEP 1 - get followers list");
-    ig.user_followers(id, paginationFollowers);  // Runs followers extrapolation
+      // converts object back to array
+      for(var i in obj){
+        var a = JSON.parse(i);
+        a.push(obj[i]);
+        result = result.concat(a);
+      };
+
+    }
+
+    return result;
+
+  }
+
+  // STEP 1 //
+  // extract followers list using paginationFollowers
+  console.log("STEP 1 - get followers list");
+  ig.user_followers(id, paginationFollowers);
 
   });
+
   };
